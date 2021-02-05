9b5ca8eeeabd1f21fda29c8849ad9c79d1cf5d5b
MrSudo777 <mr.sudo777@gmail.com>
2021-02-06 02:36:19 +0800

    Added new fie in module1 branch

---
+++ Triangle.java
@@ -0,0 +1,17 @@
+
+/*
+* Triangle.java
+*  
+*/
+public class Triangle{
+
+	private double s1;
+	private double s2;
+	private double s3;
+
+	public Triangle(double s1,double s2,double s3){
+		this.s1 =  s1;
+		this.s2 =  s2;
+		this.s3  =s3;
+	}
+}
